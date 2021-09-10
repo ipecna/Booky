4b735dad5cffb6225cc29d29d1a4ecc3d93e5f75
Semyon <ipecna@yandex.ru>
2021-09-10 11:09:04 +0300

    Initial Commit

---
+++ Booky/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Booky
+//
+//  Created by Semyon Chulkov on 10.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
